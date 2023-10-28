3c8dcd7233769f06defa0ab7fd52bdab43ade0ce
Book <Book@WIN-8CS3AUJICSU>
2023-10-28 17:44:26 +0300

    encoding

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,5 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include"resource.h"
+
+#define IDC_COMBO	1001
+#define IDC_BUTTON_APPLY	1002
+CONST CHAR* g_CURSOR[] = { "Zerg.cur", "Protoss.cur", "Terran.cur" };
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Window Class"; //имя класса окна
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -16,9 +21,12 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	wc.cbClsExtra = 0;	//Дополнительные Байты класса окна
 	wc.style = 0;	//Стиль окна
 
-	wc.hIcon = (HICON)LoadImage(hInstance, "pie_chart_icon_179859.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);				 //=LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_RAM));
-	wc.hIconSm = (HICON)LoadImage(hInstance, "idea_light_bulb_icon_179853.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);			//=LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_CPU));	//Sm - small
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	//=LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_RAM));
+	//=LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON_CPU));	//Sm - small
+	//wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = (HICON)LoadImage(hInstance, "pie_chart_icon_179859.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);				 
+	wc.hIconSm = (HICON)LoadImage(hInstance, "idea_light_bulb_icon_179853.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);			
+	wc.hCursor = (HCURSOR)LoadImage(hInstance,"Spongebob - Normal Select.ani",IMAGE_CURSOR,LR_DEFAULTSIZE,LR_DEFAULTSIZE,LR_LOADFROMFILE);
 	wc.hbrBackground = HBRUSH(COLOR_WINDOW + 1);
 
 	wc.hInstance = hInstance; //hInstance - экземпляр исполняемого файла программы в памяти
@@ -75,18 +83,88 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 //------------------------------------------
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	static HCURSOR hCursor;
 	switch (uMsg)
 	{
 	case WM_CREATE:
 	{
-		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON_LAMP));
-		HICON hIconSm = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON_PIE));
-		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
-
+		HWND hCombo = CreateWindowEx
+		(
+			NULL,
+			"ComboBox",
+			"",
+			WS_CHILD | WS_VISIBLE | CBN_DROPDOWN,
+			10, 10,
+			200, 200,
+			hwnd,
+			(HMENU)IDC_COMBO,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		for (int i = 0; i < sizeof(g_CURSOR) / sizeof(g_CURSOR[0]); i++)
+		{
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)g_CURSOR[i]);
+		}
+		HWND hMutton = CreateWindowEx
+		(
+			NULL,
+			"Button",
+			"Apply",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			220, 10,
+			100, 22,
+			hwnd,
+			(HMENU)IDC_BUTTON_APPLY,
+			GetModuleHandle(NULL),
+			NULL
+		);
 	}
 	break;
 	case WM_COMMAND:
+		switch(LOWORD(wParam))
+	case IDC_BUTTON_APPLY:
+		{
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			CHAR sz_filename[_MAX_FNAME]{};
+			CHAR sz_filepath[_MAX_PATH]= "StarCraftOriginal\\";
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_filename);
+			strcat(sz_filepath, sz_filename);
+			MessageBox(hwnd, sz_filepath, "info", MB_OK);
+			HCURSOR hCursor = (HCURSOR)LoadImage
+			(
+				GetModuleHandle(NULL),
+				sz_filepath,
+				IMAGE_CURSOR,
+				LR_DEFAULTSIZE, LR_DEFAULTSIZE,
+				LR_LOADFROMFILE
+			);
+			//SendMessage(hCombo,WM_SETCURSOR,)
+			SetCursor(hCursor);
+
+		}
 		break;
+	/*case WM_SETCURSOR:
+	{
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO);
+		int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+		CHAR sz_filename[_MAX_FNAME]{};
+		CHAR sz_filepath[_MAX_PATH] = "StarCraftOriginal\\";
+		SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_filename);
+		strcat(sz_filepath, sz_filename);
+		MessageBox(hwnd, sz_filepath, "info", MB_OK);
+		HCURSOR hCursor = (HCURSOR)LoadImage
+		(
+			GetModuleHandle(NULL),
+			sz_filepath,
+			IMAGE_CURSOR,
+			LR_DEFAULTSIZE, LR_DEFAULTSIZE,
+			LR_LOADFROMFILE
+		);
+		SetCursor(hCursor);
+
+	}
+	break;*/
 	case WM_DESTROY:PostQuitMessage(0);
 		break;
 	case WM_CLOSE:	DestroyWindow(hwnd);
