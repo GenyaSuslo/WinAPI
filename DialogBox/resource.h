7ceda5cc4b7545cd952cccbe1e97aea7fbc65e05
Book <Book@WIN-8CS3AUJICSU>
2023-10-29 17:51:16 +0300

    Add to ListBox DONE

--- DialogBox/resource.h
+++ DialogBox/resource.h
@@ -4,20 +4,22 @@
 //
 #define IDD_DIALOG1                     101
 #define IDI_ICON1                       105
+#define IDD_DIALOG_ADD                  106
 #define IDC_STATIC_LOGIN                1003
 #define IDC_EDIT_LOGIN                  1004
 #define IDC_STATIC_PASWORD              1005
 #define IDC_EDIT_PASSWORD               1006
 #define IDC_BUTTON_COPY                 1007
-#define IDC_EDIT1                       1008
+#define IDC_EDIT_ADD                    1009
+#define IDCANCEL_ADD                    1011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -5,7 +5,7 @@
 
 CONST CHAR* g_sz_VALUES[] = {"This", "is", "my", "first", "List", "Box"};
 BOOL CALLBACK DlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
-BOOL CALLBACK DlgProc1(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInsatance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShhow)
 {
@@ -50,15 +50,9 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			}
 			case IDC_BUTTON_ADD:
 			{
-				
-				CONST INT SIZE = 256;
-				CHAR sz_buffer[SIZE]{};
-				HWND text = GetDlgItem(hwnd, IDC_EDIT1);
-				HWND list = GetDlgItem(hwnd, IDC_LIST1);
-				SendMessage(text, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-				SendMessageA(list, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, (DLGPROC)DlgProcAdd, 0);	
 			}
-			break;
+				break;
 			case IDC_BUTTON_DELETE:
 			{
 				CONST INT SIZE = 256;
@@ -71,7 +65,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				if(MessageBox(hwnd, sz_message,"info",MB_OKCANCEL|MB_ICONWARNING)==1)
 				SendMessage(list, LB_DELETESTRING, i, 0);
 			}
-			break;
+				break;
 			case IDCANCEL:EndDialog(hwnd, 0);
 				break;
 			}
@@ -82,29 +76,42 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	return FALSE;
 }
-//BOOL CALLBACK DlgProc1(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-//{
-//	switch (uMsg)
-//	{
-//		case WM_INITDIALOG:
-//			break;
-//
-//		case WM_COMMAND:
-//		{
-//			switch LOWORD(wParam)
-//			{
-//			case IDOK:
-//			{
-//
-//			}
-//				break;
-//			case IDCANCEL:EndDialog(hwnd, 0);
-//				break;
-//			}
-//		}
-//
-//		case WM_CLOSE:EndDialog(hwnd, 0);
-//			break;
-//	}
-//	return FALSE;
-//}
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_INITDIALOG:
+			SetFocus(GetDlgItem(hwnd, IDC_EDIT_ADD));
+			break;
+
+		case WM_COMMAND:
+		{
+			switch LOWORD(wParam)
+			{
+			case IDOK:
+			{
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE]{};
+				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_ADD);
+				SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				HWND parent = GetParent(hwnd);//берем hwnd родительского окна
+				HWND hList = GetDlgItem(parent, IDC_LIST1);
+				if (SendMessage(hList, LB_FINDSTRING, -1, (LPARAM)sz_buffer) == CB_ERR)
+				{
+					if (strlen(sz_buffer) == 0)break;
+					SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+					EndDialog(hwnd, 0);
+				}
+				else
+					MessageBox(hwnd, "“акое значение уже есть", "info", MB_OK | MB_ICONINFORMATION);
+				break;
+			}
+			case IDCANCEL:EndDialog(hwnd, 0);
+				break;
+			}
+		}
+		break;
+		case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
