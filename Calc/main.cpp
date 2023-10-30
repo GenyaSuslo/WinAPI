a211ada6771af3831e712f810395bfd34dd6ca6b
Book <Book@WIN-8CS3AUJICSU>
2023-10-30 21:48:51 +0300

    encoding

---
+++ Calc/main.cpp
@@ -0,0 +1,93 @@
+#include<Windows.h>
+#include"resource.h"
+
+CONST CHAR g_sz_CALC[] = "Calc";
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+
+	wc.cbSize = sizeof(wc);
+	wc.cbWndExtra = 0;
+	wc.cbClsExtra = 0;
+	wc.style = 0;
+
+	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wc.hCursor = 0;
+	wc.hbrBackground = HBRUSH(COLOR_WINDOW + 1);
+	
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_sz_CALC; 
+	
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class Registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
+	INT window_width = screen_width * 0.1;
+	INT window_height = screen_height * 0.1;
+	INT start_x = screen_width / 8;
+	INT start_y = screen_height / 8;
+
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,
+		g_sz_CALC,
+		g_sz_CALC,
+		WS_OVERLAPPEDWINDOW,
+		start_x, start_y,
+		window_width, window_height,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Windows Creation Failed", "Error", MB_OK | MB_ICONINFORMATION);
+		return 0;
+	}
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+	{
+		HWND hEdit = CreateWindowEx
+		(
+			NULL,
+			"EditControl",
+			"",
+
+
+
+		)
+	}
+	break;
+
+
+	}
+
+
+}
